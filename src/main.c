8bb97e3b4834bf97ba704b85df3ddd767604f1bf
itismeherb <mikolajlaskowski7@gmail.com>
2025-12-26 12:08:10 +0100

    feat: handling arguments

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
-int main(void) {
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Please specify the file you want to compile.\n");
+        return -1;
+    }
     return 0;
 }
