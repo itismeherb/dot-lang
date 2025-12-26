ceee3f010d5c28ef9525a3bdb416b73be0b76cb0
itismeherb <mikolajlaskowski7@gmail.com>
2025-12-26 12:26:42 +0100

    feat: opening the source file

--- src/main.c
+++ src/main.c
@@ -5,5 +5,15 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please specify the file you want to compile.\n");
         return -1;
     }
+
+    FILE* source = fopen(argv[1], "r");
+    if (source) {
+        printf("Reading from: %s file\n", argv[1]);
+        fclose(source);
+    } else {
+        fprintf(stderr, "Couldn't open the %s source file\n", argv[1]);
+        return -1;
+    }
+
     return 0;
 }
